bcd40113b5998b0d86562064e2703c1f501a1699
Robert Kenny <kenoir@gmail.com>
2013-02-11 22:54:35 +0000

    added styling and jquery fade swank

--- js/src/views/penny_pincher.js
+++ js/src/views/penny_pincher.js
@@ -6,9 +6,10 @@ define(['jquery','models/money_parser'],function($,MoneyParser){
 		view.model = model;
 		view.parser = new MoneyParser();
 		
-		view.$el = $('#penny-pincher form.amount-form');
-		view.$el.submit(function(){
-			var user_input = view.$el.find('input.amount').val();
+		view.$el = $('#penny-pincher');
+
+		view.$el.find('form.amount-form').submit(function(){
+			var user_input = view.$el.find('form.amount-form input.amount').val();
 			var amount_in_pennies = view.parser.amount_in_pennies_of(user_input);
 			var change_purse = view.model.pinch(amount_in_pennies);
 
@@ -20,10 +21,15 @@ define(['jquery','models/money_parser'],function($,MoneyParser){
 	};
 
 	PennyPincherView.prototype.render = function(change_purse){
+		var view = this;
 		var coins = _.pairs(change_purse.coins)
 		var change_purse_template = $("#change-purse").html();			
-		this.$el.find('.results').html(
-			_.template(change_purse_template,{ coins: coins }));
+
+		view.$el.find('.results').fadeOut({complete:function(){
+			view.$el.find('.results').html(
+				_.template(change_purse_template,{ coins: coins }));
+			view.$el.find('.results').fadeIn();	
+		}});
 	};	
 
 
